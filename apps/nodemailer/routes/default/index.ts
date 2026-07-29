72a61c0ad3212310df2693dd0f20adbdb3ef6c6a
Hasani Rogers <contact@deificarts.com>
2026-07-29 00:56:36 -0700

    testing

--- apps/nodemailer/routes/default/index.ts
+++ apps/nodemailer/routes/default/index.ts
@@ -42,6 +42,10 @@ const transporter = nodemailer.createTransport({
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  tls: {
+    rejectUnauthorized: false
+  },
+  debug: true
 });
 
 // Validate token middleware
